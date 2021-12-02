1a50c2fbb258db5d0c5a9005ae189dc9b47eadaf
Steven Lance <44912347+stevenjlance@users.noreply.github.com>
2021-12-02 19:59:47 +0000

    Solutions added

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Create your variables here
-
+var x = 50
+var y = 50
+var speedY = 1
+var speedX = 1
 
 function setup(){
 	createCanvas(800, 600);
@@ -12,17 +15,24 @@ function setup(){
 function draw(){
   background(220);
 
-  // Reverse the direction of the logo if the image is past the right edge of the screen or past the left edge of the screen 
-	
-	
+  // reverse the direction of the logo if the image is past the right edge of the screen or past the left edge of the screen 
+	x += speedX
+	y += speedY
+	if(x > width - 200 || x < 0){
+		speedX *= -1
+		tint(random(0,255), random(0,255), random(0,255))
+	}
+	if(y > height - 150 || y < 0){
+		speedY *= -1
+		tint(random(0,255), random(0,255), random(0,255))
 
-  // Reverse the direction of the logo if the image is past the top edge of the screen or past the bottom edge of the screen 
+	}
+
+  // reverse the direction of the logo if the image is past the top edge of the screen or past the bottom edge of the screen 
 
 
   // Move the shape by changing the values of x and y every time the draw loop runs
 
-
-
   // Draw the logo at the new position.
-  image(dvdImage, 50, 50, 200, 150);
+  image(dvdImage, x, y, 200, 150);
 }
